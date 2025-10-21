5a841b01566524d32bf19f8f42d1f0c7338b24ba
Nimrah S <nimrahs@Nimrahs-MacBook-Pro.local>
2025-10-21 15:22:37 -0700

    seeing code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import matplotlib.pyplot as plt
 import os
 import threading
+import numpy as np
 
 def euclideanDistance(loc1, loc2):
     return math.sqrt((loc1[0] - loc2[0])**2 + (loc1[1] - loc2[1])**2)
@@ -104,14 +105,12 @@ with open(file_path, "w") as f:
     for i in bsfRoute:
         f.write(f"{i + 1}\n")
 
-print(f"Route image written to desk as {fileName}_SOLUTION_{bestSoFar}.jpeg\n")
-print(f"Route file written to desk as {fileName}_SOLUTION_{bestSoFar}.txt\n")
+print(f"Route image written to desktop as {fileName}_SOLUTION_{bestSoFar}.jpeg\n")
+print(f"Route file written to desktop as {fileName}_SOLUTION_{bestSoFar}.txt\n")
 
 # should allow for second input- interruption (pretty much works now)
 # must add error handling (file DNE, file in wrong format, ) error message and abort 
 # fix the 6000 meter limit error: must give warning but still create all outputs 
-# ran it, and the image shows that the algorithm did not start and end at the same node??
 # should only visit each node exactly once 
 # should get an answer in 1/4 of a second
-# must return the distance as nearest integer 
 # 10-pixel buffer between any point and an edge
